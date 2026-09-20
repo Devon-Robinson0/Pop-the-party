8f1960cfe79d632b931742d57f1b0bab8a55da49
William-Papps <william-papps@live.nmit.ac.nz>
2026-09-21 11:18:10 +1200

    Save Pop the Party progress

--- src/assets/components/js/header.js
+++ src/assets/components/js/header.js
@@ -1,6 +1,5 @@
 async function loadHeader() {
-  const response = await fetch("/src/assets/components/header.html");
-
+  const response = await fetch("../assets/components/header.html");
   const headerHTML = await response.text();
 
   document.querySelector("#header").innerHTML = headerHTML;
